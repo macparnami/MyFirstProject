ad135d10c732629586105491635be150d7015502
macparnami <mudit.parnami7@gmail.com>
2015-12-06 06:31:52 -0800

    So now i have made some conflicting and non confloictoing chnages in the master branch of my project .

--- LocalCompHelloWorld.c
+++ LocalCompHelloWorld.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main (){
 
-printf("Hello World prog from my local system\n");
+printf("Hello World prog from my local system\n now again some changes which are confilicting \n");
 printf("Here are some of the chancges that I  wanst to make\n");
 return 0;
+// some changes which are not 
 }
