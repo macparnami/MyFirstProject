3311b010a54b71714f834560c289227f533de7b3
macparnami <mudit.parnami7@gmail.com>
2015-12-07 00:21:34 -0800

    resolved conflicts

--- LocalCompHelloWorld.c
+++ LocalCompHelloWorld.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 int main (){
 
-<<<<<<< HEAD
-printf("Hello World prog from my local system\n");
-=======
 printf("Some changes which are conflicting Hello World prog from my local system\n");
->>>>>>> experimental
+
 printf("Here are some of the chancges that I  wanst to make\n");
 
 //Adding a chang that is not conflicting 
